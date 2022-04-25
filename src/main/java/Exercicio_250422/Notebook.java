0fbc212deee997dbf50bd395166b79a544be49ac
giovani89silva <giovani.tiagosilva@yahoo.com.br>
2022-04-25 20:54:50 -0300

    commit giovani 0001

---
+++ src/main/java/Exercicio_250422/Notebook.java
@@ -0,0 +1,25 @@
+package Exercicio_250422;
+
+public class Notebook extends computador {
+    
+    private float DurBateria;
+    
+    public Notebook(float DurBateria, String Marca, String Processador,
+            float capacidadeMemoria, float capacidadeHD)
+    
+    {
+        super(Marca, Processador, capacidadeMemoria, capacidadeHD);
+        this.DurBateria = DurBateria;        
+    }
+
+    public float getDurBateria() {
+        return DurBateria;
+    }
+
+    public void setDurBateria(float DurBateria) {
+        this.DurBateria = DurBateria;
+    }
+    
+    
+    
+}
---
+++ src/main/java/Exercicio_250422/Programa_PcNote.java
@@ -0,0 +1,34 @@
+package Exercicio_250422;
+
+import java.util.Scanner;
+
+public class Programa_PcNote {
+    
+    public static void main(String[] args)  {
+        
+        boolean exit = true;
+        Scanner sc = new Scanner(System.in);
+        
+        do // faz depois checka
+        {
+            System.out.println("BEM-VINDO A VOVOZONA INFORMÁTICA");
+            System.out.println("--------------------------------");
+            System.out.println("Selecione a opção: ");
+            System.out.println("1-Incluir Computador");
+            System.out.println("2-Incluir Notebook");
+            System.out.println("3-Mostrar Computador");
+            System.out.println("4-Mostrar Notebook");
+            System.out.println("5-Verificar Computador bom para jogo");
+            System.out.println("6-Verificar Notebook bom para jogo");
+            System.out.println("0- Sair");
+            
+        } while(exit);
+    
+           
+        
+        
+    }
+    
+    
+    
+}
