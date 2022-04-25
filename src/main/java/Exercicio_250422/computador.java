d5c4a07789ee607cc15610489cf996cc88fca4e0
giovani89silva <giovani.tiagosilva@yahoo.com.br>
2022-04-25 20:19:38 -0300

    Commit Aula 250422

---
+++ src/main/java/Exercicio_250422/computador.java
@@ -0,0 +1,66 @@
+package Exercicio_250422;
+
+public class computador {
+    private String Marca;
+    private String Processador;
+    private float capacidadeMemoria;
+    private float capacidadeHD;
+    private boolean bomParaJogo;
+
+    public computador() {
+    }
+    
+    public computador ( String Marca, String Processador, float capacidadeMemoria, 
+            float capacidadeHD   ) {
+        this.Marca = Marca;
+        this.Processador = Processador;
+        this.capacidadeMemoria = capacidadeMemoria;
+        this.capacidadeHD = capacidadeHD;
+    }
+
+    public String getMarca() {
+        return Marca;
+    }
+
+    public void setMarca(String Marca) {
+        this.Marca = Marca;
+    }
+
+    public String getProcessador() {
+        return Processador;
+    }
+
+    public void setProcessador(String Processador) {
+        this.Processador = Processador;
+    }
+
+    public Float getCapacidadeMemoria() {
+        return capacidadeMemoria;
+    }
+
+    public void setCapacidadeMemoria(float capacidadeMemoria) {
+        this.capacidadeMemoria = capacidadeMemoria;
+    }
+
+    public Float getCapacidadeHD() {
+        return capacidadeHD;
+    }
+
+    public void setCapacidadeHD(float capacidadeHD) {
+        this.capacidadeHD = capacidadeHD;
+    }
+
+    public boolean getBomParaJogo() {
+        return bomParaJogo;
+    }
+
+    public void setBomParaJogo(boolean bomParaJogo) {
+        if ( capacidadeMemoria > 8 && capacidadeHD > 500)
+            this.bomParaJogo = true;                 
+        
+        else 
+            this.bomParaJogo = false;             
+    } 
+   
+
+}
